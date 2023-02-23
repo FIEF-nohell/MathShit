cbe8fdef0d32a0d6642fa10e1a8a0eab8479fc93
Administrator <zivi@plan-g.at>
2023-02-23 13:35:42 +0100

    denser pointset

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const chart = new Chart(canvas, {
         datasets: [{
             label: 'y = x^2', // Label for the dataset
             data: [], // Data for the dataset
+            pointRadius: 0,
             backgroundColor: '#FF007F', // Transparent background color
             chartColor: "#333",
             borderColor: '#FF007F', // Line color
@@ -49,9 +50,10 @@ const chart = new Chart(canvas, {
 });
 
 // Generate data for the x^2 function and add it to the chart
-for (let x = -10; x <= 10; x++) {
+for (let x = -10; x <= 10; x += 0.1) {
     const y = x * x;
-    chart.data.labels.push(x);
+    chart.data.labels.push(x.toFixed(1)); // use toFixed() to round the label to 1 decimal place
     chart.data.datasets[0].data.push(y);
 }
+
 chart.update();
